6db88c7d389525b6e640161a25af6c12add5c52e
ameer <faraj.ameer@gmail.com>
2016-03-18 18:04:15 +0200

    Updated config file

--- config.js
+++ config.js
@@ -1,14 +1,6 @@
 'use strict';
 
 module.exports = {
-    //serviceName: 'testMulti',
-    //serviceGroup: 'Test Group',
-    //servicePort: 5577,
-    //extKeyRequired: false,
-    //awareness: false,
-    //requestTimeout: 30,
-    //requestTimeoutRenewal: 5,
-    //type: 'service',
-    multi: ['ex1/config.js', 'ex2/config.js', 'ex3/config.js', 'ex4/config.js'],
-    //main: 'index.js'
+    type: 'multi',
+    multi: ['ex1/config.js', 'ex2/config.js', 'ex3/config.js', 'ex4/config.js']
 };
