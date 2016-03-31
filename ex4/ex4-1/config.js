afb3ccfb269b9fff90ed7d49890115eb8a1c1cb3
ameer <faraj.ameer@gmail.com>
2016-03-31 11:54:07 +0300

    Added service

--- ex4/ex4-1/config.js
+++ ex4/ex4-1/config.js
@@ -1,16 +1,86 @@
 'use strict';
 
 module.exports = {
-    serviceName: 'EX4',
-    serviceGroup: 'Test Group',
-    servicePort: 5580,
-    extKeyRequired: false,
-    awareness: false,
+    serviceName: "jsconfTest",
+    serviceGroup: "JSConf",
+    serviceVersion: 1,
     requestTimeout: 30,
     requestTimeoutRenewal: 5,
-    type: 'daemon',
+    servicePort: 4112,
+    extKeyRequired: true,
+    type: 'service',
     prerequisites: {
         cpu: '',
         memory: ''
+    },
+	"awareness": true,
+    "errors": {
+        "600": "Database error",
+        "601": "Missing info"
+    },
+    "schema": {
+        "/addInfo": {
+            "_apiInfo":{
+                "l": "Add Info",
+                "group": "Information",
+	            "groupMain": true
+            },
+            "username": {
+                "source": ['body.username'],
+                "required": true,
+                "validation": {
+                    "type": "string",
+                    "minLength": 4,
+                    "maxLength": 8,
+                    "pattern": /^\D[\w\d_-]+$/
+                }
+            },
+            "name": {
+                "source": ['body.name'],
+                "required": false,
+                "default": "anonymous",
+                "validation": {
+                    "type": "string"
+                }
+            },
+            "email": {
+                "source": ['body.email'],
+                "required": true,
+                "validation": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "address": {"type": "string", "format": "email", "required": true},
+                            "primary": {"type": "boolean", "required": true}
+                        }
+                    },
+                    "minItems": 1,
+                    "maxItems": 5,
+                    "uniqueItems": true
+                }
+            }
+        },
+        "/getInfo": {
+            "_apiInfo": {
+                "l": "Get Info",
+                "group": "Information",
+                "groupMain": true
+            },
+            "email": {
+                "source": ['query.email'],
+                "required": true,
+                "validation": {
+                    "type": "string",
+                    "format": "email"
+                }
+            }
+        },
+        "/test/": {
+            "_apiInfo": {
+                "l": "Test",
+                "group": "Information"
+            }
+        }
     }
 };
