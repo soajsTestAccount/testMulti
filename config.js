8c988a06345374f486d7cb6555bc9f393262b77e
ameer <faraj.ameer@gmail.com>
2016-04-01 21:24:43 +0300

    updates

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     type: 'multi',
-    folders: ['ex1', '/ex2', '/ex3/', 'ex4/ex4-1/', 'daemon']
-    // folders: ['ex1', '/ex2']
+    // folders: ['ex1', '/ex2', '/ex3/', 'ex4/ex4-1/', 'daemon']
+    folders: ['ex1', '/ex2']
 };
