6bebf779794543ee5772afa9a13cf795fb70e65f
ameer <faraj.ameer@gmail.com>
2016-04-01 20:23:27 +0300

    updates

--- ex3/config.js
+++ ex3/config.js
@@ -4,8 +4,6 @@ module.exports = {
     serviceName: 'EX3',
     serviceGroup: 'Test Group',
     servicePort: 5579,
-    extKeyRequired: false,
-    awareness: false,
     requestTimeout: 30,
     requestTimeoutRenewal: 5,
     type: 'daemon',
