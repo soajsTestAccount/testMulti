b7b6fd54898af296cc83288db7c5f422d724e2e1
ameer <faraj.ameer@gmail.com>
2016-04-01 20:49:37 +0300

    removed folders paths

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 
 module.exports = {
     type: 'multi',
-    folders: ['ex1', '/ex2', '/ex3/', 'ex4/ex4-1/', 'daemon']
+    // folders: ['ex1', '/ex2', '/ex3/', 'ex4/ex4-1/', 'daemon']
+    folders: ['ex1', '/ex2']
 };
